8dbb3a44b9ff6626298ec1a94c79d077fd7a0538
KaungHtetSan29 <40780473@live.napier.ac.uk>
2025-09-22 14:55:27 +0630

    Mongo test1

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -5,32 +5,26 @@ import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.MongoCollection;
 import org.bson.Document;
 
-public class App {
-    public static void main(String[] args) {
-        // Use try-with-resources to ensure MongoClient closes properly
-        try (MongoClient mongoClient = new MongoClient("localhost", 27000)) {
-            // Get database (created automatically if it doesn’t exist)
-            MongoDatabase database = mongoClient.getDatabase("mydb");
+public class App
+{
+    public static void main(String[] args)
+    {
+        // Connect to MongoDB on local system - we're using port 27000
+        MongoClient mongoClient = new MongoClient("localhost", 27000);
+        // Get a database - will create when we use it
+        MongoDatabase database = mongoClient.getDatabase("mydb");
+        // Get a collection from the database
+        MongoCollection<Document> collection = database.getCollection("test");
+        // Create a document to store
+        Document doc = new Document("name", "Kevin Sim")
+                .append("class", "Software Engineering Methods")
+                .append("year", "2021")
+                .append("result", new Document("CW", 95).append("EX", 85));
+        // Add document to collection
+        collection.insertOne(doc);
 
-            // Get collection (created automatically if it doesn’t exist)
-            MongoCollection<Document> collection = database.getCollection("test");
-
-            // Create a document
-            Document doc = new Document("name", "Kevin Sim")
-                    .append("class", "Software Engineering Methods")
-                    .append("year", "2021")
-                    .append("result", new Document("CW", 95).append("EX", 85));
-
-            // Insert into collection
-            collection.insertOne(doc);
-
-            // Retrieve first document
-            Document myDoc = collection.find().first();
-            if (myDoc != null) {
-                System.out.println("Inserted Document: " + myDoc.toJson());
-            } else {
-                System.out.println("No document found.");
-            }
-        }
+        // Check document in collection
+        Document myDoc = collection.find().first();
+        System.out.println(myDoc.toJson());
     }
 }
